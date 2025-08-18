c54b285b85f4bdeff228410ded56b45df149cb91
Alexander Lopez <agl.alexglopez@gmail.com>
2025-08-18 15:06:51 -0700

    basic cli stuff works

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,30 @@ const maze = @import("maze.zig");
 const rdfs = @import("builders/rdfs.zig");
 const gen = @import("generator.zig");
 
+const Args = struct {
+    rows: isize = 37,
+    cols: isize = 137,
+};
+
+const row_flag: []const u8 = "-r=";
+const col_flag: []const u8 = "-c=";
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var maze_args = Args{};
     const allocator = arena.allocator();
-    var new_maze: maze.Maze = try maze.Maze.init(allocator, 37, 137);
+    for (std.os.argv) |arg| {
+        const argstr = std.mem.span(arg);
+        if (std.mem.startsWith(u8, argstr, row_flag)) {
+            const str_rows = argstr[row_flag.len..];
+            maze_args.rows = try std.fmt.parseInt(isize, str_rows, 10);
+        } else if (std.mem.startsWith(u8, argstr, col_flag)) {
+            const str_cols = argstr[row_flag.len..];
+            maze_args.cols = try std.fmt.parseInt(isize, str_cols, 10);
+        }
+    }
+    var new_maze: maze.Maze = try maze.Maze.init(allocator, maze_args.rows, maze_args.cols);
     defer {
         new_maze.deinit(allocator);
         arena.deinit();
